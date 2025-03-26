e39624b07c6d94845dacf207877e288d4faa05d0
Philipp Imhof <52650214+PhilippImhof@users.noreply.github.com>
2025-03-26 18:01:08 +0100

    MDL-85023 quiz: allow WebRTC with SEB on Mac

--- mod/quiz/accessrule/seb/db/upgrade.php
+++ mod/quiz/accessrule/seb/db/upgrade.php
@@ -34,6 +34,8 @@ require_once($CFG->dirroot  . '/mod/quiz/accessrule/seb/lib.php');
  * @return bool Result.
  */
 function xmldb_quizaccess_seb_upgrade($oldversion) {
+    global $DB;
+
     // Automatically generated Moodle v4.2.0 release upgrade line.
     // Put any upgrade step following this.
 
@@ -45,6 +47,30 @@ function xmldb_quizaccess_seb_upgrade($oldversion) {
 
     // Automatically generated Moodle v4.5.0 release upgrade line.
     // Put any upgrade step following this.
+
+    if ($oldversion < 2024121801) {
+        $dbman = $DB->get_manager();
+        $table = new xmldb_table('quizaccess_seb_quizsettings');
+        $camerafield = new xmldb_field('allowcapturecamera', XMLDB_TYPE_INTEGER, '1', null, null, null, null, 'muteonstartup');
+        // Conditionally add field.
+        if (!$dbman->field_exists($table, $camerafield)) {
+            $dbman->add_field($table, $camerafield);
+            // Now fill it with '0' for existing settings.
+            $DB->set_field('quizaccess_seb_quizsettings', 'allowcapturecamera', '0');
+        }
+
+        $microphonefield = new xmldb_field(
+            'allowcapturemicrophone', XMLDB_TYPE_INTEGER, '1', null, null, null, null, 'allowcapturecamera'
+        );
+        // Conditionally add field.
+        if (!$dbman->field_exists($table, $microphonefield)) {
+            $dbman->add_field($table, $microphonefield);
+            // Now fill it with '0' for existing settings.
+            $DB->set_field('quizaccess_seb_quizsettings', 'allowcapturemicrophone', '0');
+        }
+
+        upgrade_plugin_savepoint(true, 2024121801, 'quizaccess', 'seb');
+    }
 
     return true;
 }
